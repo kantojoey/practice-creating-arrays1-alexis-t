3ac38fe6b50c744bd1dee5ea0927eefa57334653
Alexis <atinoco999@gmail.com>
2025-07-10 15:13:44 -0700

    Compiled arrays and output array contents.

--- arrayPractice.js
+++ arrayPractice.js
@@ -1,1 +1,19 @@
+// Task 1: Create Arrays
+// Create three separate arrays using array literals for the following:
+// ● An array of 5 grocery store items.
+// ● An array of 3 numbers representing prices.
 
+let groceryList = ["Peanut butter", "Bananas", "Strawberry jelly", "Milk", "Cookies"];
+
+let priceList = [5.99, 1.50, 4.99];
+
+// Task 2: Access and Log the Values
+// Use console.log() to log the arrays.
+// 1. Log the entire array of grocery items.
+console.log(groceryList);
+// 2. Log the entire array of prices.
+console.log(priceList);
+// 3. Log three individual items of your choice.
+console.log(groceryList[0] + ", " + groceryList[1] + ", " + groceryList[2]);
+// 4. Log two prices of your choice.
+console.log(`${priceList[0]}, ${priceList[2]}`);
